379be2b30125eb04ffdb191b3015e8ed87727501
Jenny-Ha <jennyha069@gmail.com>
2018-01-08 10:24:32 -0500

    New template for slider contents

---
+++ themes/Newspaper/single_template_cna_3.php
@@ -0,0 +1,110 @@
+<?php
+// Template CNA 2 
+// Template for Magazine category
+
+locate_template('includes/wp_booster/td_single_template_vars.php', true);
+
+get_header();
+
+global $loop_sidebar_position, $td_sidebar_position, $post;
+
+$td_mod_single = new td_module_single($post);
+
+?>
+<div class="td-main-content-wrap">
+
+    <div class="td-container td-post-template-2 post-template-cna-2">
+        <article id="post-<?php echo $td_mod_single->post->ID;?>" class="<?php echo join(' ', get_post_class());?>" <?php echo $td_mod_single->get_item_scope();?>>
+            <div class="td-crumb-container"><?php echo td_page_generator::get_single_breadcrumbs($td_mod_single->title); ?></div>
+            <div class="td-pb-row">
+                <div class="td-pb-span12">
+                    <div class="td-post-header">
+                        <?php echo $td_mod_single->get_category(); ?>
+                        <header class="td-post-title">
+                            <?php echo $td_mod_single->get_title();?>
+                            <?php if (!empty($td_mod_single->td_post_theme_settings['td_subtitle'])) { ?>
+                                <p class="td-post-sub-title"><?php echo $td_mod_single->td_post_theme_settings['td_subtitle'];?></p>
+                            <?php } ?>
+                            <!-- Sharing Top -->
+                            <?php echo $td_mod_single->get_social_sharing_top();?>
+                        </header>
+                        <div class="wrapper-feature">
+                            <?php
+                                // override the default featured image by the templates (single.php and home.php/index.php - blog loop)
+                                $value = get_field( "name" );
+                                if (!empty($value)) {
+                                    echo do_shortcode('[rev_slider '.$value.']');
+                                } else {
+                                    echo 'Coloque el nombre del slider para mostrar las imágenes';
+                                }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- /.td-pb-row -->
+
+            <div class="td-pb-row">
+                <?php
+
+                //the default template
+                switch ($loop_sidebar_position) {
+                    default:
+                        ?>
+                            <div class="td-pb-span8 td-main-content" role="main">
+                                <div class="td-ss-main-content">
+                                    <?php
+                                    locate_template('loop-single-cna-2.php', true);
+                                    comments_template('', true);
+                                    ?>
+                                </div>
+                            </div>
+                            <div class="sidebar-post-cna td-pb-span4 td-main-sidebar" role="complementary">
+                                <div class="td-ss-main-sidebar">
+                                    <?php get_sidebar(); ?>
+                                </div>
+                            </div>
+                        <?php
+                        break;
+
+                    case 'sidebar_left':
+                        ?>
+                        <div class="td-pb-span8 td-main-content <?php echo $td_sidebar_position; ?>-content" role="main">
+                            <div class="td-ss-main-content">
+                                <?php
+                                locate_template('loop-single-2.php', true);
+                                comments_template('', true);
+                                ?>
+                            </div>
+                        </div>
+	                    <div class="sidebar-post-cna td-pb-span4 td-main-sidebar" role="complementary">
+		                    <div class="td-ss-main-sidebar">
+			                    <?php get_sidebar(); ?>
+		                    </div>
+	                    </div>
+                        <?php
+                        break;
+
+                    case 'no_sidebar':
+                        td_global::$load_featured_img_from_template = 'td_1068x0';
+                        ?>
+                        <div class="td-pb-span12 td-main-content" role="main">
+                            <div class="td-ss-main-content">
+                                <?php
+                                locate_template('loop-single-2.php', true);
+                                comments_template('', true);
+                                ?>
+                            </div>
+                        </div>
+                        <?php
+                        break;
+
+                }
+                ?>
+            </div> <!-- /.td-pb-row -->
+        </article> <!-- /.post -->
+    </div> <!-- /.td-container -->
+</div> <!-- /.td-main-content-wrap -->
+
+<?php
+
+get_footer();
