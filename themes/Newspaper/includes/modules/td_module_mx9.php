0614ed34c42f7687a22a3dcc945ef8e0d54991e4
Jenny-Ha <jennyha069@gmail.com>
2018-01-19 14:34:49 -0500

    Fix style magazine grid

--- themes/Newspaper/includes/modules/td_module_mx9.php
+++ themes/Newspaper/includes/modules/td_module_mx9.php
@@ -25,6 +25,9 @@ class td_module_mx9 extends td_module {
                     <div class="td-big-grid-meta">
                         <?php if (td_util::get_option('tds_category_module_mx9') == 'yes') { echo $this->get_category(); }?>
                         <?php echo $this->get_title();?>
+                        <div class="myExcerpt">
+                        <?php echo $this->get_excerpt(); ?>
+                        </div>
                     </div>
                 </div>
             </div>
