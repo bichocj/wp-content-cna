d6cca55327fd98070972a8eeeaf7592d5639d548
luisaGonzales <luisagc711@gmail.com>
2018-02-19 18:47:07 -0500

    indent header-bxslider

--- themes/Newspaper/parts/header/header-style-6.php
+++ themes/Newspaper/parts/header/header-style-6.php
@@ -83,43 +83,43 @@ Header style 6 - CNA
                     <div class="td-block-row">
                     <?php $newsQuery = new WP_Query( array('post_type' => 'post', 'post_status' => 'publish', 'posts_per_page' => 4) );?>
                     <?php if($newsQuery->have_posts()) : ?>
-                    <div id="header-bxslider">
-                        <?php while($newsQuery->have_posts()) : $newsQuery->the_post(); ?>
-                        <!-- td-block-span4 -->
-                        <div class="td-block-span4">
-                            <div class="td_module_mx4 td_module_wrap td-animation-stack">
-                                <div class="td-module-image">
-                                    <div class="td-module-thumb">
-                                        <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>">
+                        <div id="header-bxslider">
+                            <?php while($newsQuery->have_posts()) : $newsQuery->the_post(); ?>
+                            <!-- td-block-span4 -->
+                            <div class="td-block-span4">
+                                <div class="td_module_mx4 td_module_wrap td-animation-stack">
+                                    <div class="td-module-image">
+                                        <div class="td-module-thumb">
+                                            <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>">
+                                            <?php 
+                                                    $thumbID = get_post_thumbnail_id( $post->ID );
+                                                    $imgDestacada = wp_get_attachment_image_src( $thumbID, 'large' ); // Thumbnail, medium, large, full
+                                                    $imgTitle = get_the_title();
+                                                    echo '<img width="218" height="150" class="entry-thumb td-animation-stack-type0-1" src="'.$imgDestacada[0].'"
+                                                    alt="" title="'.$imgTitle.'">'
+                                            ?>
+                                            </a>
+                                        </div>
                                         <?php 
-                                                $thumbID = get_post_thumbnail_id( $post->ID );
-                                                $imgDestacada = wp_get_attachment_image_src( $thumbID, 'large' ); // Thumbnail, medium, large, full
-                                                $imgTitle = get_the_title();
-                                                echo '<img width="218" height="150" class="entry-thumb td-animation-stack-type0-1" src="'.$imgDestacada[0].'"
-                                                alt="" title="'.$imgTitle.'">'
-                                        ?>
-                                        </a>
-                                    </div>
-                                    <?php 
-                                        foreach((get_the_category()) as $key => $category){
-                                            $category_name = $category->name;
-                                            $category_id = get_cat_ID( $category_name ); 
-                                            $category_link = get_category_link( $category_id ); 
-                                            if($key == 0) {
-                                                echo '<a href="'. $category_link.'" class="td-post-category">'. $category_name .'</a>';
+                                            foreach((get_the_category()) as $key => $category){
+                                                $category_name = $category->name;
+                                                $category_id = get_cat_ID( $category_name ); 
+                                                $category_link = get_category_link( $category_id ); 
+                                                if($key == 0) {
+                                                    echo '<a href="'. $category_link.'" class="td-post-category">'. $category_name .'</a>';
+                                                }
                                             }
-                                        }
-                                    ?>
-                                </div>
-
-                                <h3 class="entry-title td-module-title">
-                                    <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-                                </h3>
-                            </div>
-                        </div> 
-                        <!-- ./td-block-span4 -->
-                        <?php endwhile; ?>
+                                        ?>
                                     </div>
+
+                                    <h3 class="entry-title td-module-title">
+                                        <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+                                    </h3>
+                                </div>
+                            </div> 
+                            <!-- ./td-block-span4 -->
+                            <?php endwhile; ?>
+                        </div>
                     <?php endif; wp_reset_postdata(); ?>
                     </div>
                 </div>
@@ -136,41 +136,41 @@ Header style 6 - CNA
                         <div class="l6-row clearfix">
                             <?php $news6Query = new WP_Query( array('post_type' => 'post', 'post_status' => 'publish', 'posts_per_page' => 6) );?>
                             <?php if($news6Query->have_posts()) : ?>
-                                <?php while($news6Query->have_posts()) : $news6Query->the_post(); ?>
-                                <!-- l6-block-->
-                                <div class="l6-block2">
-                                    <div class="l6-module td-animation-stack">
-                                        <div class="td-module-image">
-                                            <div class="td-module-thumb">
-                                                <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>">
-                                                <?php 
-                                                        $thumbID = get_post_thumbnail_id( $post->ID );
-                                                        $imgDestacada = wp_get_attachment_image_src( $thumbID, array(218,150) ); // Thumbnail, medium, large, full
-                                                        $imgTitle = get_the_title();
-                                                        echo '<img width="218" height="150" class="entry-thumb td-animation-stack-type0-1" src="'.$imgDestacada[0].'"
-                                                        alt="" title="'.$imgTitle.'">'
-                                                ?>
-                                                </a>
-                                            </div>
-                                            <!-- <?php 
-                                                foreach((get_the_category()) as $key => $category){
-                                                    $category_name = $category->name;
-                                                    $category_id = get_cat_ID( $category_name ); 
-                                                    $category_link = get_category_link( $category_id ); 
-                                                    if($key == 0) {
-                                                        echo '<a href="'. $category_link.'" class="td-post-category">'. $category_name .'</a>';
+                                    <?php while($news6Query->have_posts()) : $news6Query->the_post(); ?>
+                                    <!-- l6-block-->
+                                    <div class="l6-block2">
+                                        <div class="l6-module td-animation-stack">
+                                            <div class="td-module-image">
+                                                <div class="td-module-thumb">
+                                                    <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>">
+                                                    <?php 
+                                                            $thumbID = get_post_thumbnail_id( $post->ID );
+                                                            $imgDestacada = wp_get_attachment_image_src( $thumbID, array(218,150) ); // Thumbnail, medium, large, full
+                                                            $imgTitle = get_the_title();
+                                                            echo '<img width="218" height="150" class="entry-thumb td-animation-stack-type0-1" src="'.$imgDestacada[0].'"
+                                                            alt="" title="'.$imgTitle.'">'
+                                                    ?>
+                                                    </a>
+                                                </div>
+                                                <!-- <?php 
+                                                    foreach((get_the_category()) as $key => $category){
+                                                        $category_name = $category->name;
+                                                        $category_id = get_cat_ID( $category_name ); 
+                                                        $category_link = get_category_link( $category_id ); 
+                                                        if($key == 0) {
+                                                            echo '<a href="'. $category_link.'" class="td-post-category">'. $category_name .'</a>';
+                                                        }
                                                     }
-                                                }
-                                            ?> -->
-                                        </div>
+                                                ?> -->
+                                            </div>
 
-                                        <h5 class="entry-title td-module-title">
-                                            <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-                                        </h5>
-                                    </div>
-                                </div> 
-                                <!-- ./l6-block  -->
-                                <?php endwhile; ?>
+                                            <h5 class="entry-title td-module-title">
+                                                <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+                                            </h5>
+                                        </div>
+                                    </div> 
+                                    <!-- ./l6-block  -->
+                                    <?php endwhile; ?>
                             <?php endif; wp_reset_postdata(); ?>
                         </div>
                     </div>
