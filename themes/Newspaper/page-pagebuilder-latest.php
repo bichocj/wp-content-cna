64b150c916c8fa52d93800941828a28d01728835
luisaGonzales <luisagc711@gmail.com>
2018-03-08 13:17:30 -0500

    add class

--- themes/Newspaper/page-pagebuilder-latest.php
+++ themes/Newspaper/page-pagebuilder-latest.php
@@ -186,7 +186,7 @@ if(!empty($post->post_content)) { //show this only when we have content
 <?php } ?>
 
 <!-- Sidebar Lo mas Visto -->
-<div class="visible-xs">
+<div class="visible-xs widget-most-viewed">
     <?php dynamic_sidebar( 'sidebar-mas-visto' ); ?>
 </div>
 <!-- Sidebar lo mas Visto end -->
