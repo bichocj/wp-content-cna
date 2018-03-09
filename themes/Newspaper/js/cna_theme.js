a6e444eda605d475559b89b9bcbdd207edb28d77
luisaGonzales <luisagc711@gmail.com>
2018-03-09 15:02:51 -0500

    functions icons

--- themes/Newspaper/js/cna_theme.js
+++ themes/Newspaper/js/cna_theme.js
@@ -11,22 +11,70 @@
 //Delete hours 
 //Get all dates, compare exception, put a point.
 
-const deleteHours = (dates, element, point, replace) => {
-    let text = "";
-    let index = 0;
+// const deleteHours = (dates, element, point, replace) => {
+//     let text = "";
+//     let index = 0;
+//     if (dates.length > 0) {
+//         for (let i in dates) {
+//             text = dates[i].innerHTML;
+//             if (text != element) {
+//                 index = text.indexOf(point);
+//                 dates[i].innerHTML = text.slice(0, index);
+//             } else {
+//                 dates[i].innerHTML = replace;
+//             }
+//         }
+//     }
+// }
+
+jQuery(document).ready(function () {
+    // Delete Hours
+    deleteHours(document.getElementsByClassName("wpcna-polls-dates"), 'Resultados: No Expiry', "@", "Encuesta Vigente");
+    // Resize plugin de Facebook
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=443271375714375";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    jQuery(document).ready(function ($) {
+        $(window).bind("load resize", function () {
+            setTimeout(function () {
+                var container_width = $('#container').width();
+                $('#container').html('<div class="fb-page" ' +
+                    'data-href="http://www.facebook.com/IniciativaAutoMat"' +
+                    ' data-width="' + container_width + '" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="http://www.facebook.com/IniciativaAutoMat"><a href="http://www.facebook.com/IniciativaAutoMat">Auto*Mat</a></blockquote></div></div>');
+                FB.XFBML.parse();
+            }, 100);
+        });
+    });
+});
+
+
+function deleteHours(dates, element, point, replace) {
+    var dates = dates;
+    console.log("deleteHours");
+    console.log(dates);
+    console.log(dates.length);
+
+    var text = "";
+    var index = 0;
     if (dates.length > 0) {
-        for (let i in dates) {
+        for (var i in dates) {
             text = dates[i].innerHTML;
             if (text != element) {
                 index = text.indexOf(point);
                 dates[i].innerHTML = text.slice(0, index);
             } else {
                 dates[i].innerHTML = replace;
+                console.log("else");
             }
         }
     }
 }
-deleteHours(document.getElementsByClassName("wpcna-polls-dates"), 'Resultados: No Expiry', "@", "Encuesta Vigente");
 
 // Change class 
 function changeClass(question) {
@@ -65,27 +113,5 @@ jQuery(function () {
         pager: false,
         infiniteLoop: false,
         hideControlOnEnd: true
-    });
-});
-
-// Resize plugin de Facebook
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=443271375714375";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-jQuery(document).ready(function ($) {
-    $(window).bind("load resize", function () {
-        setTimeout(function () {
-            var container_width = $('#container').width();
-            $('#container').html('<div class="fb-page" ' +
-                'data-href="http://www.facebook.com/IniciativaAutoMat"' +
-                ' data-width="' + container_width + '" data-tabs="timeline" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="http://www.facebook.com/IniciativaAutoMat"><a href="http://www.facebook.com/IniciativaAutoMat">Auto*Mat</a></blockquote></div></div>');
-            FB.XFBML.parse();
-        }, 100);
     });
 });
